a284df56beca3725420be95a08013bc14ec17cd0
yanchhuong <yanchhuongksign@gmail.com>
2019-01-25 16:00:31 +0700

    upadate commit

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const db = require('./app/config/db.config.js');
 
 const Customer = db.customers;
   
- force: true will drop the table if it already exists
-// db.sequelize.sync({force: true}).then(() => {
+// force: true will drop the table if it already exists
+ db.sequelize.sync({force: true}).then(() => {
    console.log('Drop and Resync with { force: true }');
      initial();
      });
